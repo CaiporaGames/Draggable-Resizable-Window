9df32c60e406badccc34dc468ae2b4cae05fe6de
caiporagames <caiporagames@yahoo.com.br>
2022-07-25 16:04:31 +0100

    Fisrt Commit
    This is a resizable and draggable window.
    
    We can put flags that allow we to still use the background window but it is not implemented here. Look the video on youtube

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+//Here is the link to the project of the modal: https://github.com/mazdik/ng-modal
+export class AppComponent {
+  title = 'resize-window';
+}
